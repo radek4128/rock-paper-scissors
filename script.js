d81f07ec36786e13872911885555af4ae26efca7
radek4128 <radek4128@gmail.com>
2026-07-08 14:42:57 +0200

    Add logic to play 5 rounds by default
    - add function playGame
    - update getUserChoice function to display round nr and
      score in prompt window

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getComputerChoice() {
     random_number = Math.random(0, 3);
-    console.log(random_number);
     if (random_number < 0.33) {
         return 'rock';
     } else if (random_number >=0.33 && random_number < 0.66) {
@@ -10,8 +9,12 @@ function getComputerChoice() {
     }
 }
 
-function getUserChoice() {
-    choice = prompt("Please enter one of the following: Rock, Paper, Scissors", "");
+function getUserChoice(round) {
+    if (round == 1) {
+        choice = prompt("Round: 1\nPlease enter one of the following: Rock, Paper, Scissors", "");
+    } else {
+        choice = prompt(`Round: ${round}\nYou: ${userScore} Computer: ${computerScore}\nPlease enter one of the following: Rock, Paper, Scissors`, "");
+    }
     return choice;
 }
 
@@ -23,8 +26,8 @@ const msg3 = "rock beats scissors";
 let userScore = 0;
 let computerScore = 0;
 
-function playRound() {
-    const userChoice = getUserChoice().toLowerCase();
+function playRound(round) {
+    const userChoice = getUserChoice(round).toLowerCase();
     const computerChoice = getComputerChoice();
     console.log(`User choice: ${userChoice}`);
     console.log(`Computer choice: ${computerChoice}`);
@@ -72,6 +75,25 @@ function playRound() {
 }
 
 
-playRound();
-console.log(`Computer score: ${computerScore}`);
-console.log(`User score: ${userScore}`);
+
+
+function playGame() {
+    for (let i=0; i < 5; i++) {
+        playRound(i+1);
+    }
+    // Compare user and computer score
+    if (userScore > computerScore) {
+        message = "You won the game!";
+    } else {
+        message = "You lost the game :(";
+    }
+
+    should_continue = prompt(`${message}\nYou: ${userScore} Computer: ${computerScore}\nDo you wanna play another one? y/n"`);
+    if (should_continue === 'y') {
+        userScore = 0;
+        computerScore = 0;
+        playGame()
+    }
+}
+
+playGame();
